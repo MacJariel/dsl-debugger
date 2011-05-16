30e81560d9a5cf2ea3a5f224ceb81347845b6839
MacJariel <MacJariel@gmail.com>
2011-05-17 00:43:12 +0200

    Added project that adds Java target platform.

---
+++ org.macjariel.dsl.debugger.java/src/org/macjariel/dsl/debugger/java/JavaPlatformFactory.java
@@ -0,0 +1,24 @@
+package org.macjariel.dsl.debugger.java;
+
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.macjariel.dsl.debugger.platform.ITargetPlatformFactory;
+
+public class JavaPlatformFactory implements ITargetPlatformFactory {
+
+	public JavaPlatformFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ILaunchConfigurationDelegate createLaunchDelegate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
